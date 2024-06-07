699572ab5b9b1515cd7c44022263b519d7143c59
takuya0831 <2201145016ga@cyber-u.ac.jp>
2024-06-08 01:13:36 +0900

    App mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "**********";
+const uri = "mongodb+srv://2201145016ga:YwW9B6vappg7Kqxm@test.evgosjn.mongodb.net/?retryWrites=true&w=majority&appName=test";
 const client = new MongoClient(uri);
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
