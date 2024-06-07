ce6ef4d4b7163d200ff3240682b4620068842d9e
takuya0831 <2201145016ga@cyber-u.ac.jp>
2024-06-07 11:00:40 +0900

    App mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,16 @@
-test02
+var express = require('express');
+var router = express.Router();
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = "cu test";
+const client = new MongoClient(uri);
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
+// idが１のドキュメントを取得
+const query = { id: 2 };
+const note = await notes.findOne(query);
+res.json(note);
+})
+module.exports = router;
