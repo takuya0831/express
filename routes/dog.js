f310eb799c312f9e2239b218f27378e26ec5d467
takuya0831 <2201145016ga@cyber-u.ac.jp>
2024-06-08 17:06:36 +0900

    dog api 1706

--- routes/dog.js
+++ routes/dog.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 router.get('/', async (req, res) => {
-request('https://dog.ceo/api/breeds/image/random', function (error, response,
+//ここのアドレスを変更　20240608_1700
+	request('https://dog.ceo/api/breeds/image/random', function (error, response,
 body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
