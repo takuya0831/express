daf00a4724aa888d425b61bb79dc8cb50127cbe4
takuya0831 <2201145016ga@cyber-u.ac.jp>
2024-06-08 13:05:05 +0900

    app cat api 1304

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-//const uri = "CU 20240608_1301";
+const uri = "CU 20240608_1304";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
