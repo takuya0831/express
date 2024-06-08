9b47b4730f2777e782496ef91a6b5352831d8de6
takuya0831 <2201145016ga@cyber-u.ac.jp>
2024-06-08 16:57:20 +0900

    dog api

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
-var notesRouter = require('./routes/notes');
-var catRouter = require('./routes/cat'); 
+//var notesRouter = require('./routes/notes');
+var catRouter = require('./routes/cat');
+var dogRouter = require('./routes/dog'); 
 
 var app = express();
 
@@ -25,8 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
-app.use('/notes', notesRouter); 
+//app.use('/notes', notesRouter); 
 app.use('/cat', catRouter);
+app.use('/dog', dogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
